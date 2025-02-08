abef0135690e9ddb51a3449dec8f37855be98ffc
Harrison Page <harrison.page@harrison.page>
2025-02-08 14:22:23 -0800

    here we go

--- src/main/java/love/toad/Finger.java
+++ src/main/java/love/toad/Finger.java
@@ -5,37 +5,48 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.logging.Logger;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-import java.util.HashMap;
-import java.util.UUID;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Map;
 
-import love.toad.PlayerActivityListener;
-
-public class Finger extends JavaPlugin implements CommandExecutor {
-    private final HashMap<UUID, Long> lastActiveTime = new HashMap<>();
-    private final HashMap<UUID, Double> distanceWalked = new HashMap<>();
-    private JSONObject playerData;
+public class Finger extends JavaPlugin {
+    private static String PLUGIN_NAME = "Finger";
+    private static String VERSION = "1.0.0";
+    private static String VERSION_STRING = PLUGIN_NAME + '/' + VERSION;
+    private static String GITHUB_URL = "https://github.com/harrisonpage/Finger";
     private final File dataFile = new File(getDataFolder(), "player_data.json");
+    private PlayerActivityListener playerActivityListener;
+    private File configFile;
+    private FileConfiguration config;
+    private String htmlPlayerReport;
+    private String serverName;
+    Logger log = Logger.getLogger("Minecraft");
+    long startTime;
 
     @Override
     public void onEnable() {
+        startTime = System.currentTimeMillis();
         if (!getDataFolder().exists()) {
             getDataFolder().mkdirs();
         }
+        loadConfig();
+        playerActivityListener = new PlayerActivityListener(dataFile);
         if (!dataFile.exists()) {
             try {
                 dataFile.createNewFile();
@@ -47,56 +58,29 @@ public class Finger extends JavaPlugin implements CommandExecutor {
             }
         }
         this.getCommand("finger").setExecutor(this);
-        loadPlayerData();
-        getServer().getPluginManager().registerEvents(new PlayerActivityListener(lastActiveTime, distanceWalked, playerData, dataFile), this);
+        playerActivityListener.loadPlayerData();
+        getServer().getPluginManager().registerEvents(playerActivityListener, this);
+        log.info("[" + VERSION_STRING + "] Enabled: " + GITHUB_URL);
+
+        if (isHtmlReportEnabled()) {
+            // initial first write
+            writeReport();
+            // then write HTML report every 60s
+            Bukkit.getScheduler().runTaskTimerAsynchronously(Bukkit.getPluginManager().getPlugin("Finger"), this::writeReport, 1200L, 1200L);
+            log.info("[" + VERSION_STRING + "] Periodically writing " + htmlPlayerReport + " file");
+        }
     }
 
     @Override
     public void onDisable() {
-        savePlayerData();
-    }
-
-    private void loadPlayerData() {
-        if (!dataFile.exists()) {
-            playerData = new JSONObject();
-            return;
-        }
-        try (FileReader reader = new FileReader(dataFile)) {
-            JSONParser parser = new JSONParser();
-            Object obj = parser.parse(reader);
-            if (obj instanceof JSONObject) {
-                playerData = (JSONObject) obj;
-                for (Object key : playerData.keySet()) {
-                    JSONObject playerEntry = (JSONObject) playerData.get(key);
-                    double distance = ((Number) playerEntry.getOrDefault("distance_walked", 0.0)).doubleValue();
-                    distanceWalked.put(UUID.fromString((String) key), distance);
-                }
-            } else {
-                playerData = new JSONObject();
-            }
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-            playerData = new JSONObject();
-        }
-    }
-
-    private void savePlayerData() {
-        for (UUID uuid : distanceWalked.keySet()) {
-            JSONObject playerEntry = (JSONObject) playerData.getOrDefault(uuid.toString(), new JSONObject());
-            playerEntry.put("distance_walked", distanceWalked.get(uuid));
-            playerData.put(uuid.toString(), playerEntry);
-        }
-        try (FileWriter writer = new FileWriter(dataFile)) {
-            writer.write(playerData.toJSONString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        playerActivityListener.savePlayerData();
+        log.info("[" + VERSION_STRING + "] Disabled");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "This command can only be used by players.");
+            sender.sendMessage(ChatColor.RED + "This command can only be used by players");
             return true;
         }
 
@@ -108,7 +92,7 @@ public class Finger extends JavaPlugin implements CommandExecutor {
         } else {
             target = Bukkit.getPlayerExact(args[0]);
             if (target == null) {
-                requester.sendMessage(ChatColor.RED + "Player " + args[0] + " is not online.");
+                requester.sendMessage(ChatColor.RED + "Player " + args[0] + " is not online");
                 return true;
             }
         }
@@ -117,23 +101,93 @@ public class Finger extends JavaPlugin implements CommandExecutor {
         return true;
     }
 
+    private void loadConfig() {
+        configFile = new File(getDataFolder(), "config.yml");
+
+        if (!configFile.exists()) {
+            log.info("[" + VERSION_STRING + "] Initial configuration file " + configFile + " created");
+            saveDefaultConfig();
+        }
+        copyTemplateFile();
+
+        config = YamlConfiguration.loadConfiguration(configFile);
+        htmlPlayerReport = config.getString("html_player_report", "");
+        serverName = config.getString("server_name", "My Server");
+
+        if (htmlPlayerReport.isEmpty()) {
+            htmlPlayerReport = null; // Disable feature if not set
+        }
+    }
+
+    public void saveDefaultConfig() {
+        configFile = new File(getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            config = new YamlConfiguration();
+            config.set("html_player_report", "");
+            config.set("server_name", "My Server");
+            try {
+                config.save(configFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void copyTemplateFile() {
+        File templateFile = new File(getDataFolder(), "template.html");
+        if (!templateFile.exists()) {
+            log.info("[" + VERSION_STRING + "] Initial HTML template file created");
+            getDataFolder().mkdirs();
+            try (InputStream in = getResource("template.html");
+                 FileOutputStream out = new FileOutputStream(templateFile)) {
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = in.read(buffer)) > 0) {
+                    out.write(buffer, 0, length);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public FileConfiguration getConfigData() {
+        return config;
+    }
+
+    public String getHtmlPlayerReport() {
+        return htmlPlayerReport;
+    }
+
+    public boolean isHtmlReportEnabled() {
+        return htmlPlayerReport != null;
+    }
+
+
+    private String s(int i) {
+        return i == 1 ? "" : "s";
+    }
+
     private void sendFingerReport(Player requester, Player target) {
-        long idleTime = System.currentTimeMillis() - lastActiveTime.getOrDefault(target.getUniqueId(), System.currentTimeMillis());
+        long idleTime = System.currentTimeMillis() - playerActivityListener.lastActiveTime.getOrDefault(target.getUniqueId(), System.currentTimeMillis());
         String idleStatus = idleTime > 0 ? (idleTime / 60000) + "m" : "Active";
 
-        double walked = distanceWalked.getOrDefault(target.getUniqueId(), 0.0);
+        double walked = playerActivityListener.distanceWalked.getOrDefault(target.getUniqueId(), 0.0);
+        double damageDealt = playerActivityListener.damageDealt.getOrDefault(target.getUniqueId(), 0.0);
+        int mobsKilled = playerActivityListener.mobsKilled.getOrDefault(target.getUniqueId(), 0);
+        int blocksMined = playerActivityListener.blocksMined.getOrDefault(target.getUniqueId(), 0);
+        int deaths = playerActivityListener.deaths.getOrDefault(target.getUniqueId(), 0);
 
         requester.sendMessage(ChatColor.GOLD + "[Finger] " + ChatColor.YELLOW + target.getName() + " (Level " + target.getLevel() + ")");
-        requester.sendMessage("- Distance Walked: " + String.format("%.2f", walked) + " meters");
-
         Location loc = target.getLocation();
-        requester.sendMessage("- Location: " + loc.getWorld().getName() + " (XYZ: " + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")");
-        requester.sendMessage("- Ping: " + target.getPing() + "ms Idle: " + idleStatus);
-        requester.sendMessage("- Mode: " + target.getGameMode() + " Env: " + target.getWorld().getEnvironment());
-        requester.sendMessage("- Health: " + Math.round(target.getHealth()) + "/" + Math.round(target.getMaxHealth()) + " Hunger: " + target.getFoodLevel() + "/20");
+        requester.sendMessage("- Location: " + loc.getWorld().getName() + " (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ") " + target.getGameMode());
+        requester.sendMessage("- Ping: " + target.getPing() + "ms, Idle: " + idleStatus);
+        requester.sendMessage("- Health: " + Math.round(target.getHealth()) + "/" + Math.round(target.getHealthScale()) + " Hunger: " + target.getFoodLevel() + "/20");
+        requester.sendMessage("- Stats: Walked " + String.format("%.2f", walked) + " meters, mined " + blocksMined + " block" + s(blocksMined));
+        requester.sendMessage("- Combat: " + mobsKilled + " kill" + s(mobsKilled) + ", " + Math.round(damageDealt) + " damage dealt, " + deaths + " death" + s(deaths));
 
         ItemStack heldItem = target.getInventory().getItemInMainHand();
-        requester.sendMessage("- Holding: " + getItemName(heldItem));
+        requester.sendMessage("- Holding: " + getItemDetails(heldItem));
 
         sendArmorInfo(requester, target, "Helmet", target.getInventory().getHelmet());
         sendArmorInfo(requester, target, "Chestplate", target.getInventory().getChestplate());
@@ -141,12 +195,142 @@ public class Finger extends JavaPlugin implements CommandExecutor {
         sendArmorInfo(requester, target, "Boots", target.getInventory().getBoots());
     }
 
-    private void sendArmorInfo(Player requester, Player target, String piece, ItemStack item) {
-        String itemName = getItemName(item);
-        requester.sendMessage("- " + piece + ": " + itemName);
+    private void writeReport() {
+        File templateFile = new File(getDataFolder(), "template.html");
+        String template = loadTemplate(templateFile);
+
+        if (template == null) {
+            return; // Abort if template couldn't be loaded
+        }
+
+        // Extract player-card template
+        String playerCardTemplate = extractPlayerCardTemplate(template);
+        if (playerCardTemplate == null) {
+            log.warning("[Finger] Player card template not found in template.html");
+            return;
+        }
+
+        StringBuilder playerEntries = new StringBuilder();
+        for (Player target : Bukkit.getOnlinePlayers()) {
+            long idleTime = System.currentTimeMillis() - playerActivityListener.lastActiveTime.getOrDefault(target.getUniqueId(), System.currentTimeMillis());
+            String idleStatus = idleTime > 0 ? (idleTime / 60000) + "m" : "Active";
+
+            double walked = playerActivityListener.distanceWalked.getOrDefault(target.getUniqueId(), 0.0);
+            double damageDealt = playerActivityListener.damageDealt.getOrDefault(target.getUniqueId(), 0.0);
+            int mobsKilled = playerActivityListener.mobsKilled.getOrDefault(target.getUniqueId(), 0);
+            int blocksMined = playerActivityListener.blocksMined.getOrDefault(target.getUniqueId(), 0);
+            int deaths = playerActivityListener.deaths.getOrDefault(target.getUniqueId(), 0);
+
+            Location loc = target.getLocation();
+            String uuid = target.getUniqueId().toString();
+
+            String entry = playerCardTemplate.replace("{PLAYER_NAME}", target.getName())
+                                   .replace("{UUID}", uuid)
+                                   .replace("{LEVEL}", String.valueOf(target.getLevel()))
+                                   .replace("{WORLD}", loc.getWorld().getName())
+                                   .replace("{X}", String.valueOf(loc.getBlockX()))
+                                   .replace("{Y}", String.valueOf(loc.getBlockY()))
+                                   .replace("{Z}", String.valueOf(loc.getBlockZ()))
+                                   .replace("{GAMEMODE}", target.getGameMode().toString())
+                                   .replace("{PING}", String.valueOf(target.getPing()))
+                                   .replace("{IDLE}", idleStatus)
+                                   .replace("{HEALTH}", Math.round(target.getHealth()) + "/" + Math.round(target.getHealthScale()))
+                                   .replace("{HUNGER}", target.getFoodLevel() + "/20")
+                                   .replace("{WALKED}", String.format("%.2f", walked))
+                                   .replace("{BLOCKS_MINED}", String.valueOf(blocksMined))
+                                   .replace("{MOBS_KILLED}", String.valueOf(mobsKilled))
+                                   .replace("{DAMAGE_DEALT}", String.valueOf(Math.round(damageDealt)))
+                                   .replace("{DEATHS}", String.valueOf(deaths))
+                                   .replace("{HELD_ITEM}", getItemDetails(target.getInventory().getItemInMainHand()))
+                                   .replace("{HELMET}", getItemDetails(target.getInventory().getHelmet()))
+                                   .replace("{CHESTPLATE}", getItemDetails(target.getInventory().getChestplate()))
+                                   .replace("{LEGGINGS}", getItemDetails(target.getInventory().getLeggings()))
+                                   .replace("{BOOTS}", getItemDetails(target.getInventory().getBoots()));
+
+            playerEntries.append(entry).append("\n");
+        }
+
+        String playerEntriesString = playerEntries.length() > 0 ? playerEntries.toString() : "<p style='text-align: center;'>Nobody Online</p>";
+        String finalHtml = template.replace("{PLAYER_ENTRIES}", playerEntriesString);
+
+        String serverVersion = Bukkit.getServer().getVersion();
+        String bukkitVersion = Bukkit.getServer().getBukkitVersion();
+
+        LocalDateTime now = LocalDateTime.now();
+        String timestamp = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        long uptimeMillis = System.currentTimeMillis() - startTime;
+        long uptimeSeconds = uptimeMillis / 1000;
+        long uptimeMinutes = uptimeSeconds / 60;
+        long uptimeHours = uptimeMinutes / 60;
+        String uptimeFormatted = uptimeHours + "h " + (uptimeMinutes % 60) + "m " + (uptimeSeconds % 60) + "s";
+
+        long maxMemory = Runtime.getRuntime().maxMemory() / 1024 / 1024; // MB
+        long allocatedMemory = Runtime.getRuntime().totalMemory() / 1024 / 1024;
+        long freeMemory = Runtime.getRuntime().freeMemory() / 1024 / 1024;
+        long usedMemory = allocatedMemory - freeMemory;
+
+
+        finalHtml = finalHtml.replace("{VERSION_STRING}", VERSION_STRING)
+            .replace("{TIMESTAMP}", timestamp)
+            .replace("{SERVER_NAME}", serverName)
+            .replace("{SERVER_VERSION}", serverVersion)
+            .replace("{BUKKIT_VERSION}", bukkitVersion)
+            .replace("{SERVER_UPTIME}", uptimeFormatted)
+            .replace("{SERVER_MEMORY_MAX}", String.valueOf(maxMemory))
+            .replace("{SERVER_MEMORY_ALLOCATED}", String.valueOf(allocatedMemory))
+            .replace("{SERVER_MEMORY_FREE}", String.valueOf(freeMemory))
+            .replace("{SERVER_MEMORY_USED}", String.valueOf(usedMemory))
+            .replace("{GITHUB_URL}", GITHUB_URL);
+
+        File file = new File(getHtmlPlayerReport());
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(finalHtml);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private String getItemName(ItemStack item) {
-        return (item != null && item.getType() != Material.AIR) ? item.getType().name().replace("_", " ") : "None";
+    private String extractPlayerCardTemplate(String template) {
+        String startTag = "<script id=\"player-card-template\" type=\"text/template\">";
+        String endTag = "</script>";
+        int startIndex = template.indexOf(startTag);
+        int endIndex = template.indexOf(endTag, startIndex);
+        if (startIndex == -1 || endIndex == -1) {
+            return null;
+        }
+        return template.substring(startIndex + startTag.length(), endIndex).trim();
+    }
+
+    private String loadTemplate(File file) {
+        if (!file.exists()) {
+            log.warning("Template file not found: " + file.getPath());
+            return null;
+        }
+        try {
+            return new String(Files.readAllBytes(file.toPath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void sendArmorInfo(Player requester, Player target, String piece, ItemStack item) {
+        requester.sendMessage("- " + piece + ": " + getItemDetails(item));
+    }
+
+    private String getItemDetails(ItemStack item) {
+        if (item == null || item.getType() == Material.AIR) return "None";
+        StringBuilder details = new StringBuilder(item.getType().name().replace("_", " "));
+        ItemMeta meta = item.getItemMeta();
+        if (meta != null && meta.hasEnchants()) {
+            details.append(" (Enchanted: ");
+            for (Map.Entry<Enchantment, Integer> entry : meta.getEnchants().entrySet()) {
+                details.append(entry.getKey().getKey().getKey()).append(" ").append(entry.getValue()).append(", ");
+            }
+            details.setLength(details.length() - 2);
+            details.append(")");
+        }
+        return details.toString();
     }
 }
--- src/main/java/love/toad/PlayerActivityListener.java
+++ src/main/java/love/toad/PlayerActivityListener.java
@@ -1,31 +1,43 @@
 package love.toad;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
 import java.util.HashMap;
 import java.util.UUID;
 import org.bukkit.entity.Player;
 import org.bukkit.Location;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class PlayerActivityListener implements Listener {
-    private final HashMap<UUID, Long> lastActiveTime;
-    private final HashMap<UUID, Double> distanceWalked;
-    private final JSONObject playerData;
+    private JSONObject playerData;
     private final File dataFile;
+    public final HashMap<UUID, Long> lastActiveTime = new HashMap<>();
+    public final HashMap<UUID, Double> distanceWalked = new HashMap<>();
+    public final HashMap<UUID, Double> damageDealt = new HashMap<>();
+    public final HashMap<UUID, Integer> mobsKilled = new HashMap<>();
+    public final HashMap<UUID, Integer> blocksMined = new HashMap<>();
+    public final HashMap<UUID, Integer> deaths = new HashMap<>();
 
-    public PlayerActivityListener(HashMap<UUID, Long> lastActiveTime, HashMap<UUID, Double> distanceWalked, JSONObject playerData, File dataFile) {
-        this.lastActiveTime = lastActiveTime;
-        this.distanceWalked = distanceWalked;
-        this.playerData = playerData;
+    public PlayerActivityListener(File dataFile) {
         this.dataFile = dataFile;
+        this.playerData = new JSONObject();
+        // Schedule periodic data saving every 60 seconds
+        Bukkit.getScheduler().runTaskTimerAsynchronously(Bukkit.getPluginManager().getPlugin("Finger"), this::savePlayerData, 1200L, 1200L);
     }
 
     @EventHandler
@@ -45,13 +57,83 @@ public class PlayerActivityListener implements Listener {
         }
 
         lastActiveTime.put(uuid, System.currentTimeMillis());
-        savePlayerData();
     }
 
-    private void savePlayerData() {
+    @EventHandler
+    public void onEntityDamage(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player) {
+            Player player = (Player) event.getDamager();
+            UUID uuid = player.getUniqueId();
+            damageDealt.put(uuid, damageDealt.getOrDefault(uuid, 0.0) + event.getDamage());
+        }
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (event.getEntity().getKiller() != null) {
+            Player player = event.getEntity().getKiller();
+            UUID uuid = player.getUniqueId();
+            mobsKilled.put(uuid, mobsKilled.getOrDefault(uuid, 0) + 1);
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+        blocksMined.put(uuid, blocksMined.getOrDefault(uuid, 0) + 1);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        UUID uuid = player.getUniqueId();
+        deaths.put(uuid, deaths.getOrDefault(uuid, 0) + 1);
+    }
+
+    public void loadPlayerData() {
+        try (FileReader reader = new FileReader(dataFile)) {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(reader);
+            if (obj instanceof JSONObject) {
+                playerData = (JSONObject) obj;
+                for (Object key : playerData.keySet()) {
+                    JSONObject playerEntry = (JSONObject) playerData.get(key);
+                    UUID uuid = UUID.fromString((String) key);
+
+                    double distance = ((Number) playerEntry.getOrDefault("distance_walked", 0.0)).doubleValue();
+                    distanceWalked.put(uuid, distance);
+
+                    double damage = ((Number) playerEntry.getOrDefault("damage_dealt", 0.0)).doubleValue();
+                    damageDealt.put(uuid, damage);
+
+                    int mobs = ((Number) playerEntry.getOrDefault("mobs_killed", 0)).intValue();
+                    mobsKilled.put(uuid, mobs);
+
+                    int blocks = ((Number) playerEntry.getOrDefault("blocks_mined", 0)).intValue();
+                    blocksMined.put(uuid, blocks);
+
+                    int deathCount = ((Number) playerEntry.getOrDefault("deaths", 0)).intValue();
+                    deaths.put(uuid, deathCount);
+                }
+            } else {
+                playerData = new JSONObject();
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            playerData = new JSONObject();
+        }
+    }
+
+    public void savePlayerData() {
         for (UUID uuid : distanceWalked.keySet()) {
             JSONObject playerEntry = (JSONObject) playerData.getOrDefault(uuid.toString(), new JSONObject());
             playerEntry.put("distance_walked", distanceWalked.get(uuid));
+            playerEntry.put("damage_dealt", damageDealt.getOrDefault(uuid, 0.0));
+            playerEntry.put("mobs_killed", mobsKilled.getOrDefault(uuid, 0));
+            playerEntry.put("blocks_mined", blocksMined.getOrDefault(uuid, 0));
+            playerEntry.put("deaths", deaths.getOrDefault(uuid, 0));
+
             playerData.put(uuid.toString(), playerEntry);
         }
         try (FileWriter writer = new FileWriter(dataFile)) {
